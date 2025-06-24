419287660ddb7ce786928935c7de542c96da4711
erdxd <tytalexsey@mail.ru>
2025-06-24 20:22:29 +1200

    add

---
+++ account/accaount.go
@@ -0,0 +1,62 @@
+package account
+
+import (
+	"errors"
+	"fmt"
+	"math/rand/v2"
+	"net/url"
+	"time"
+)
+
+// композиция(наследование)
+
+var letterruns = []rune("qwertyuiop[]asdfghjkl;'zxcvbnm,.1234567890-=")
+
+func (acc *Account) GeneratePassword(n int) {
+	ald := make([]rune, n)
+
+	for i := range ald {
+		ald[i] = letterruns[rand.IntN(len(letterruns))]
+
+	}
+	acc.password = string(ald)
+}
+
+func (acc *Account) Outputhassword() {
+	fmt.Println(acc.login, "-логин")
+	fmt.Println(acc.password, "-пароль")
+	fmt.Println(acc.URL, "-ЮРЛ")
+}
+
+func NewAccountWithTimeStamp(login, password, URLstring string) (*AccountWithTimeStamp, error) {
+
+	_, err := url.ParseRequestURI(URLstring)
+	if err != nil {
+
+		return nil, errors.New("Invalid_URL")
+	}
+
+	Newacc := &AccountWithTimeStamp{
+		createdTime: time.Now(),
+		updateTime:  time.Now(),
+		Account: Account{
+			login:    login,
+			password: password,
+			URL:      URLstring,
+		},
+	}
+
+	return Newacc, nil
+
+}
+
+type Account struct {
+	login    string
+	password string
+	URL      string
+}
+type AccountWithTimeStamp struct {
+	createdTime time.Time
+	updateTime  time.Time
+	Account
+}
--- test.go
+++ test.go
@@ -1,50 +1,10 @@
 package main
 
 import (
-	"errors"
 	"fmt"
-	"math/rand/v2"
-	"net/url"
+
+	"github.com/Erdxd/password/account"
 )
-
-var letterruns = []rune("qwertyuiop[]asdfghjkl;'zxcvbnm,.1234567890-=")
-
-func (acc *account) generatePassword(n int) {
-	ald := make([]rune, n)
-
-	for i := range ald {
-		ald[i] = letterruns[rand.IntN(len(letterruns))]
-
-	}
-	acc.password = string(ald)
-}
-
-func (acc *account) outputhassword() {
-	fmt.Println(acc.login, "-логин", acc.password, "-пароль", acc.URL, "-URL")
-}
-func newAccount(login, password, URLstring string) (*account, error) {
-
-	_, err := url.ParseRequestURI(URLstring)
-	if err != nil {
-
-		return nil, errors.New("Invalid_URL")
-	}
-
-	Newacc := &account{
-		login:    login,
-		password: password,
-		URL:      URLstring,
-	}
-
-	return Newacc, nil
-
-}
-
-type account struct {
-	login    string
-	password string
-	URL      string
-}
 
 func main() {
 
@@ -52,9 +12,7 @@ func main() {
 	password := promtData("Введите пароль")
 	URL := promtData("Введите URL")
 	var err error
-
-	var MYaccount *account
-	MYaccount, err = newAccount(login, password, URL)
+	MYaccount, err := account.NewAccountWithTimeStamp(login, password, URL)
 	if err != nil {
 		fmt.Println("неверный формат URL")
 		return
@@ -65,10 +23,11 @@ func main() {
 	}
 	if password == "" {
 		fmt.Println("нету пароля,генерируем...")
-		MYaccount.generatePassword(10)
+		MYaccount.GeneratePassword(10)
 	}
 
-	MYaccount.outputhassword()
+	MYaccount.Outputhassword()
+
 }
 
 func promtData(prompt string) string {
